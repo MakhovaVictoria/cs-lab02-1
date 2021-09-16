b2d46f324b80f6be35655d34f182ea05ce4c1ee2
Bob (MakhovaVA) <bob@example.com>
2021-09-16 23:49:05 +0300

    code: максимум и минимум

--- main.cpp
+++ main.cpp
@@ -5,14 +5,15 @@ using namespace std;
 int main()
 {
     cout << "Enter A and B: ";
-int a, b, c;
+int a, b, c,d;
 cin >> a >> b;
-if (a<b) c=a;
-else c=b;
+if (a>b) {c=a; d=b;}
+else {c=b; d=a;};
 cout << "A + B = " << a + b << '\n'
      << "A - B = " << a - b << '\n'
      << "A * B = " << a * b << '\n'
      << "A / B = " << a / b << '\n'
-     << "Min = " << c << '\n';
+     << "Max = " << c << '\n'
+     << "Min = " << d << '\n';
     return 0;
 }
