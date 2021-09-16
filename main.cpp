97604e8966b65bc69e5e8c75bb7d120f2fb40b0d
Alice (MakhovaVA) <alice@example.com>
2021-09-17 00:56:45 +0300

    final

--- main.cpp
+++ main.cpp
@@ -5,26 +5,15 @@ using namespace std;
 int main()
 {
     cout << "Enter A and B: ";
-<<<<<<< HEAD
-int a, b, c,d;
+int a, b, c, d;
 cin >> a >> b;
 if (a>b) {c=a; d=b;}
 else {c=b; d=a;};
-=======
-int a, b, c;
-cin >> a >> b;
-if (a>b) c=a;
-else c=b;
->>>>>>> 2520a24001604eb0e4a264476498e11291365d4b
 cout << "A + B = " << a + b << '\n'
      << "A - B = " << a - b << '\n'
      << "A * B = " << a * b << '\n'
      << "A / B = " << a / b << '\n'
-<<<<<<< HEAD
      << "Max = " << c << '\n'
      << "Min = " << d << '\n';
-=======
-     << "Max = " << c << '\n';
->>>>>>> 2520a24001604eb0e4a264476498e11291365d4b
     return 0;
 }
